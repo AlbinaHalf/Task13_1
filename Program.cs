a9f59e00be018cb86a11a310ce97060f500d7b95
Albina <albina.half@gmail.com>
2022-07-18 15:38:33 +0500

    Task 13 has been solved

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу, которая выводит
+// третью цифру заданного числа или сообщает, что третьей
+// цифры нет.
+
+Console.WriteLine("Etner the number");
+int number = Convert.ToInt32(Console.ReadLine());
+number = Math.Abs(number);
+if (number < 100)
+{
+   Console.WriteLine("There is no third digit"); 
+}
+if (number > 99 && number < 1000)
+{
+    Console.WriteLine(number % 10);
+}
